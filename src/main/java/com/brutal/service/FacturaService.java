98c2b80344d43adbf8ee0562aa51b409a77807de
JerryRivera609 <jerry609rivera@gmail.com>
2025-06-30 21:35:25 -0500

    acutalizando

--- src/main/java/com/brutal/service/FacturaService.java
+++ src/main/java/com/brutal/service/FacturaService.java
@@ -117,7 +117,7 @@ public class FacturaService extends GenericService <Factura, Long> {
         OffsetDateTime inicioDeDia = fecha.atStartOfDay().atOffset(ZoneOffset.UTC);
         OffsetDateTime finDeDia = fecha.atTime(LocalTime.MAX).atOffset(ZoneOffset.UTC);
 
-        return facturaRepository.findByFechaHoraBetween(inicioDeDia, finDeDia);
+        return facturaRepository.findByFechaHoraBetween(gitinicioDeDia, finDeDia);
     }
 
 
